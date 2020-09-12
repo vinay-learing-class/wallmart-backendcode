cbfddaa61b6094de5973b62098fbd1d2c4533a6f
root <root@ip-172-31-50-171.ec2.internal>
2020-09-12 17:48:05 +0000

    second file

---
+++ subtraction.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "enter the two number"
+read -p "enter first number:"  num1
+read -p "enetr second number:" num2
+
+echo  "Subtraction of two number is : " `expr $num1 - $num2`
+
